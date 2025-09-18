e00c60f28a69d814155429a068703f08e844d1dc
Kumar Manik <141612023+KumarManik44@users.noreply.github.com>
2025-09-18 11:55:15 +0530

    Create __init__.py

---
+++ data_ingestion/__init__.py
@@ -0,0 +1,5 @@
+from .news_api_client import NewsAPIClient
+from .rss_processor import RSSProcessor
+from .article_storage import ArticleStorage, Article
+
+__all__ = ['NewsAPIClient', 'RSSProcessor', 'ArticleStorage', 'Article']
